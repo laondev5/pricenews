0d21259b01a3f1192f01079bfb4e6ae9fb5b541d
laon <ekohabraham5@gmail.com>
2023-12-13 09:47:51 +0000

    first pricenews commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import './globals.css'
+import { Navbar } from '@/components'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({subsets: ['latin'],
+weight:['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Price new',
+  description: 'Track product prices effortlessly and save money on you online shopping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+          <main   className='max-w-10xl mx-auto'>
+            <Navbar />
+          {children}
+          </main>
+        </body>
+    </html>
+  )
+}
